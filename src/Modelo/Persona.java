9cd6ed30a24444f4e10e61f0a7f952e06031fe43
crisjona24 <cristobal.rios@unl.edu.ec>
2021-02-01 22:09:46 -0500

    Inicio de las clases Conexion

--- src/Modelo/Persona.java
+++ src/Modelo/Persona.java
@@ -10,5 +10,62 @@ package Modelo;
  * @author PC
  */
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String correo;
+    private String numeroTelefono;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String cedula, String correo, String numeroTelefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+    
     
 }
